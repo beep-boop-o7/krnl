7363e1383f251c18cbf874ba49221db7a8b16c2d
beep-boop-o7 <andreas@lorenzen.us>
2026-04-22 10:32:20 +0200

    Update k08isrsem.ino

--- examples/k08isrsem/k08isrsem.ino
+++ examples/k08isrsem/k08isrsem.ino
@@ -30,7 +30,7 @@ volatile int ISRoverflow = 0;
 unsigned long lastISR = 0;
 
 #if defined (__AVR_ATmega2560__) || defined (__AVR_ATmega1280__) || defined(__AVR_ATmega2561__)
-ISR(INT4_vect, ISR_NAKED) // digital pin 2 onmega
+ISR(INT4_vect, ISR_NAKED) // digital pin 2 on mega
 #endif
 
 //UNO  set interrupt on pin 2
@@ -106,8 +106,8 @@ char a[STK];
 
 
 void setup() {
-	// for debugging - only on uno eq- If Mega etc use PORTA(which is pn 22,23,...)
-	// see http://kom.aau.dk/~jdn/edu/doc/arduino/mega.png
+	// for debugging - only on uno eq- If Mega etc use PORTA(which is pin 22,23,...)
+	// see https://jensd.dk/doc/arduino/mega.png
 	
 	Serial.begin(115200);
 	delay(500);
@@ -115,11 +115,15 @@ void setup() {
 	Serial.println("\n\njust bef init part");
 	
 	
-	k_init(1, 1, 0); // 2 task, 1 semaphores, 0 messaegQueues */
+	k_init(1, 1, 0);
+	//     |  |  |--- num of mg Queues (0)
+	//     |  |----- num of semaphores (1)
+	//     |------------- num of tasks (1)
+	
+	// priority: lower number has higher priority than higher number
+	pTask1 = k_crt_task(task1, 15, a,  STK);
 
-pTask1 = k_crt_task(task1, 15, a,  STK);
-
-syncSem = k_crt_sem(0, 10); // 1: start value, 10: max value (clipping)
+	syncSem = k_crt_sem(0, 10); // 1: start value, 10: max value (clipping)
 
 
 pinMode(2, INPUT_PULLUP);  // on uno - - a button to ground (or wire) every time it goes low vupti an interrupt
