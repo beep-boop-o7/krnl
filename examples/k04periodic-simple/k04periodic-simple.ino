3a7859a59596af86a8fdd64add8eef4296ec5de3
beep-boop-o7 <andreas@lorenzen.us>
2026-04-22 10:05:49 +0200

    Update k04periodic-simple.ino

--- examples/k04periodic-simple/k04periodic-simple.ino
+++ examples/k04periodic-simple/k04periodic-simple.ino
@@ -9,7 +9,7 @@ void t1()
   k_set_sem_timer(s1, 100); // let krnl signal sem s1 every 100 tick == 100 msec
   
   while (1) {
-    k_wait(s1, 0); //wait until  a kick comes   parm 0 means waith until signal - so no timeout
+    k_wait(s1, 0); //wait until a kick comes parm 0 means waith until signal - so no timeout
     digitalWrite(13, ! digitalRead(13));
   }
 }
@@ -47,16 +47,16 @@ void setup()
   while (! Serial) ;
   pinMode(13, OUTPUT);
 
-  k_init(2, 1, 0); // init with space for one task
-  //         |--- no of mg Queues (0)
-  //       |----- no of semaphores (0)
-  //     |------- no of tasks (2)
+  k_init(2, 1, 0); // init with space for two tasks and one semaphore
+  //     |  |  |--- num of mg Queues (0)
+  //     |  |----- num of semaphores (1)
+  //     |------------- num of tasks (2)
 
-  // priority low number higher priority than higher number
-  p1 = k_crt_task(t1, 10, stk1,STK); // t1 as task, priority 10, 100 B stak
-  p2 = k_crt_task(t2, 11, stk2,STK); // t2 as task, priority 11 == lower than t1, 100 B stak
+	// priority: lower number has higher priority than higher number
+  p1 = k_crt_task(t1, 10, stk1, STK); // t1 as task, priority 10, 100 B stak
+  p2 = k_crt_task(t2, 11, stk2, STK); // t2 as task, priority 11 == lower than t1, 100 B stak
   
-  s1 = k_crt_sem(0, 10); // crt sem
+  s1 = k_crt_sem(0, 10); // create semaphore
 
   res = k_start(); // 1 milli sec tick speed
   // you will never return from k_start
