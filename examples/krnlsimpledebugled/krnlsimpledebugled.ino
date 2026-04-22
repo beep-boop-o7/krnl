2efa284f0c239956f90a372c6454588c4bf859be
beep-boop-o7 <andreas@lorenzen.us>
2026-04-22 11:25:05 +0200

    Update krnlsimpledebugled.ino

--- examples/krnlsimpledebugled/krnlsimpledebugled.ino
+++ examples/krnlsimpledebugled/krnlsimpledebugled.ino
@@ -31,7 +31,7 @@ void setup()
 
   k_init(3, 0, 0); // init with space for three tasks
 
-	// priority: lower number has higher priority than higher number
+  // priority: lower number has higher priority than higher number
   p1 = k_crt_task(tgeneric, 10, st1, STK);  // t1 as task, priority 10, 100 B stak
   p2 = k_crt_task(tgeneric, 11, st2,  STK); // t1 as task, priority 11, 100 B stak
   p3 = k_crt_task(tgeneric, 12, st3, STK);  // t1 as task, priority 12, 100 B stak
